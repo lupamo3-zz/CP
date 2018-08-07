e4ff008101b81e7ab1c84dc5a1a4ded002961bc1
lan <lupamo93@gmail.com>
2018-08-07 16:14:41 +0300

    templates, creation of models, navbar creation

--- invekeapp/models.py
+++ invekeapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Business(models.Model):
+    investidea=models.CharField(max_length=51)
+    post = HTMLField(blank=False)
+    locationjpg=models.ImageField(upload_to = 'jpg/')
