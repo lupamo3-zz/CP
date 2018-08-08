ce2133837bced38512fc6db37d9ca4d2df00ae3c
lan <lupamo93@gmail.com>
2018-08-08 09:38:47 +0300

    add a backgrond image, minimum css

--- invekeapp/models.py
+++ invekeapp/models.py
@@ -6,3 +6,9 @@ class Business(models.Model):
     investidea=models.CharField(max_length=51)
     post = HTMLField(blank=False)
     locationjpg=models.ImageField(upload_to = 'jpg/')
+
+    def __str__(self):
+        return self.investidea
+
+    class Meta:
+        ordering=['investidea']
